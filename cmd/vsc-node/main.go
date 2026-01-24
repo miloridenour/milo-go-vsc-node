759718a8836575318d5433fed64af40832303a6c
techcoderx <git@techcoderx.com>
2026-01-24 13:24:40 +0800

    print network name

--- cmd/vsc-node/main.go
+++ cmd/vsc-node/main.go
@@ -60,6 +60,7 @@ func main() {
 
 	hiveURI := hiveApiUrl.Get().HiveURI
 
+	fmt.Println("Network:", parsedArgs.network)
 	fmt.Println("MONGO_URL", os.Getenv("MONGO_URL"))
 	fmt.Println("HIVE_API", hiveURI)
 	fmt.Println("Git Commit", announcements.GitCommit)
